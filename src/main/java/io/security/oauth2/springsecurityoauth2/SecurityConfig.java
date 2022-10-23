cdc93efab95440fbed8ad7318bb7ee99637acff8
yoonhwanSS <wesaq11@gmail.com>
2022-10-23 18:37:38 +0900

    AuthenticationEntryPoint Process

--- src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/SecurityConfig.java
@@ -2,18 +2,58 @@ package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 public class SecurityConfig {
 
     // WebSecurityConfigurer 가 Deprecated되면서 Bean을 생성해서 사용하는 방식으로 변경되었다.
+    /*
+        SecurityFilterChain은 Prototype으로 등록되기 때문에 여러개의 객체를 만들어서 쓸 수 있다.
+        2개의 SecurityFilterChain 을 등록하면 2개 모두 등록된다.
+     */
     @Bean
+    @Order(0)
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
+        http.httpBasic();
+//        http.apply(new CustomSecurityConfigurer().setFlag(true));
+        return http.build();
+    }
+
+    @Bean
+    @Order(1)
+    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
         http.formLogin();
-        http.apply(new CustomSecurityConfigurer().setFlag(true));
+//        http.apply(new CustomSecurityConfigurer().setFlag(true));
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain securityFilterChain3(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().authenticated();
+
+        // Custom한 EntryPoint 를 등록하면 커스텀한 객체가 우선시 동작하고 이외의 EntryPoint는 무시된다.
+        // 커스텀하지 않앗다면 formLogin 방식이 우선 진행된다.
+        http.formLogin();
+        http.httpBasic();
+        http.exceptionHandling().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                System.out.println("...custom");
+            }
+        });
 
         return http.build();
     }
