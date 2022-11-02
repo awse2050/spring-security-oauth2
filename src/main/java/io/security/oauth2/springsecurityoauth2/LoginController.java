46b06cfc7a31ee1f91905e2d6b1a82a0f52c8631
yoonhwanSS <wesaq11@gmail.com>
2022-11-02 23:38:06 +0900

    keyCloak OAuth2Client Configuration

---
+++ src/main/java/io/security/oauth2/springsecurityoauth2/LoginController.java
@@ -0,0 +1,15 @@
+package io.security.oauth2.springsecurityoauth2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("/loginPage")
+    public String loginPage() {
+        return "loginPage";
+    }
+
+}
--- src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/OAuth2ClientConfig.java
@@ -1,6 +1,7 @@
 package io.security.oauth2.springsecurityoauth2;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,8 +11,16 @@ public class OAuth2ClientConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated();
-        http.oauth2Login();
+        /*
+            Customizer 를 사용하면 커스터마이징을 하기위한 설정을 할 수 있다.
+         */
+        http.authorizeRequests(authRequest ->
+                authRequest
+                        .antMatchers("/loginPage").permitAll()
+                        .anyRequest().authenticated()
+        );
+//        http.oauth2Login(oauth2 -> oauth2.loginPage("/loginPage"));
+        http.oauth2Login(Customizer.withDefaults()); // 기본설정으로 변경
         return http.build();
     }
 }
